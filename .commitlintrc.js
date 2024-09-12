5238a8264d82d426bfb1607226975b9e1f5f153a
Atsuya Yamagami <my2502@icloud.com>
2024-09-13 03:24:37 +0900

    Revert "chore: commitlintを導入 #1"
    This reverts commit 719fe60d46711c53b23cd4041e44dee9e832923f.

--- .commitlintrc.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('@commitlint/types').UserConfig} */
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'subject-case': [0, 'never', []], // 日本語を用いるため無効化
-    'references-empty': [2, 'never'],
-  },
-};
