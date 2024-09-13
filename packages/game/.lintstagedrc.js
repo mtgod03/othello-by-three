cd7039b153195928a0a06ca7691749f0f9ac2ae9
Atsuya Yamagami <my2502@icloud.com>
2024-09-13 23:45:42 +0900

    chore: .lintstagedrc.jsを作成

---
+++ packages/game/.lintstagedrc.js
@@ -0,0 +1,14 @@
+const jsFiles = '*.{?([cm])js,jsx}';
+const tsFiles = '*.{?([cm])ts,tsx}';
+const otherFiles = `!({${jsFiles},${tsFiles}})`;
+
+const tscCommand = () => 'tsc --noEmit';
+const eslintCommand = 'eslint --fix .';
+const prettierCommand = 'prettier --write --ignore-unknown';
+
+/** @type {import('lint-staged').Config} */
+module.exports = {
+  [jsFiles]: [eslintCommand, prettierCommand],
+  [tsFiles]: [tscCommand, eslintCommand, prettierCommand],
+  [otherFiles]: prettierCommand,
+};
