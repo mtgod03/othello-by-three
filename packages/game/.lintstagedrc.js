2a054e4be298fe2141322864dc1257a4fc6e85fe
Atsuya Yamagami <my2502@icloud.com>
2024-09-18 23:52:08 +0900

    fix: lint-stagedのESLintの設定を修正

--- packages/game/.lintstagedrc.js
+++ packages/game/.lintstagedrc.js
@@ -3,7 +3,7 @@ const tsFiles = '*.{?([cm])ts,tsx}';
 const otherFiles = `!({${jsFiles},${tsFiles}})`;
 
 const tscCommand = () => 'tsc --noEmit';
-const eslintCommand = 'eslint --fix .';
+const eslintCommand = 'eslint --fix';
 const prettierCommand = 'prettier --write --ignore-unknown';
 
 /** @type {import('lint-staged').Config} */
