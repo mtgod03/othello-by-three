39b2d6f784d3bac06a1be4aa2fca3c535bc79f42
Atsuya Yamagami <my2502@icloud.com>
2024-10-12 21:08:35 +0900

    chore: Nextの追加したい設定を記載

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  compiler: {
+    // TODO: turbopackがこの機能に対応したら有効化する
+    // removeConsole: true,
+  },
+  experimental: {
+    // TODO: turbopackがこの機能に対応したら有効化する
+    // typedRoutes: true,
+  },
+};
 
 export default nextConfig;
